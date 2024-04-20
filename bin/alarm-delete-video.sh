bff122dd17a1d9e8c7cf20542252b0bd1c9be5c7
perttuta <root@alarm.home>
2024-04-20 13:28:38 +0000

    Record video support

--- bin/alarm-delete-video.sh
+++ bin/alarm-delete-video.sh
@@ -3,5 +3,8 @@
 # Specify the directory path
 directory="/var/cache/alarm-video"
 
-# Find files older than 10 minutes and delete them
-find "$directory" -type f -mmin +10 -exec rm -f {} \;
+while true; do
+  # Find files older than 10 minutes and delete them
+  find "$directory" -type f -mmin +10 -exec rm -f {} \;
+  sleep 600
+done
