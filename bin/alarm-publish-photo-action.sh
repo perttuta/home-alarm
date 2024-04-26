d95d69ba351cfb48a0d452eb184a649d14db1040
perttuta <root@alarm.home>
2024-04-26 23:40:33 +0300

    Fixed photo sending

--- bin/alarm-publish-photo-action.sh
+++ bin/alarm-publish-photo-action.sh
@@ -10,6 +10,9 @@ do
         echo -e "\n"$(date)" Publishing new alarm photo"
         # send it via Telegram
         curl --silent -X POST -H "Content-Type:multipart/form-data" -F "chat_id=$ENV_TG_CHAT_ID" -F document=@"$FILE_PHOTO" "https://api.telegram.org/bot$ENV_TG_BOT_TOKEN/sendDocument"
+        # for some reason a short wait is needed, because otherwise curl fails
+        sleep 1
+        echo -e "\n"$(date)" Publishing done"
         rm "$FILE_PHOTO"
     fi
     sleep 1  # Wait 1 second until recheck
