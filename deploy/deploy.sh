d8c7b7deb87d00e387bc37542928d0618315f02d
Perttu Taskinen <perttu@nowhere.fi>
2024-10-23 22:39:28 +0300

    Deploy script 2

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update
-apt install curl supervisor ffmpeg mosquitto-clients jq git
+apt install -y curl supervisor ffmpeg mosquitto-clients jq git
 timedatectl set-timezone "Europe/Helsinki"
