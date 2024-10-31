d966c02834a3983e6bab99298dd3161aba555981
Perttu Taskinen <perttu@nowhere.fi>
2024-10-31 22:32:36 +0200

    Deploy script 10

--- bin/alarm-record-video.sh
+++ bin/alarm-record-video.sh
@@ -5,4 +5,4 @@ source /usr/bin/alarm-util.sh
 set -a; source "${ENV_FILE}"; set +a
 
 log "Starting video recording"
-/usr/bin/ffmpeg -loglevel error -i rtsp://${CAMERA_USERNAME}:${CAMERA_PASSWORD}@${CAMERA_RTSP_URL} -c:v copy -map 0 -f segment -segment_time 10 -segment_format mp4 "${ALARM_VIDEO_DIR}"/"${ALARM_VIDEO_FILE_PREFIX}"%%04d.mp4
+/usr/bin/ffmpeg -loglevel error -i rtsp://${CAMERA_USERNAME}:${CAMERA_PASSWORD}@${CAMERA_RTSP_URL} -c:v copy -map 0 -f segment -segment_time 10 -segment_format mp4 "${ALARM_VIDEO_DIR}"/"${ALARM_VIDEO_FILE_PREFIX}"%04d.mp4
