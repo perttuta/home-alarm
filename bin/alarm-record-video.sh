d7aa7b9db4bc99c0f0e3596ace5717767d268082
Perttu Taskinen <perttu@nowhere.fi>
2025-12-06 16:57:44 +0200

    Restart video on unexpected exit

--- bin/alarm-record-video.sh
+++ bin/alarm-record-video.sh
@@ -12,5 +12,5 @@ if [ "$RECORDING_MODE" = "continuous" ]; then
     /usr/bin/ffmpeg -loglevel error -i rtsp://${CAMERA_USERNAME}:${CAMERA_PASSWORD}@${CAMERA_RTSP_URL} -an -c:v copy -map 0 -f segment -segment_time 10 -segment_format mp4 "${ALARM_VIDEO_DIR}"/"${ALARM_VIDEO_FILE_PREFIX}"%04d.mp4
 else
     log "On-demand recording mode - service not needed"
-    exit 0
+    exit 123
 fi
